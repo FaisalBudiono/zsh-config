357bd79ccc07d0a39430f871e6443ec292365004
Faisal Budiono <ucul.project@gmail.com>
2023-05-09 12:54:38 +0700

    Move WSL alias to wsl-alias module

--- modules/alias.zsh
+++ modules/alias.zsh
@@ -1,8 +1,3 @@
-# This is specific to WSL 2. If the WSL 2 VM goes rogue and decides not to free
-# up memory, this command will free your memory after about 20-30 seconds.
-#   Details: https://github.com/microsoft/WSL/issues/4166#issuecomment-628493643
-alias wslrefresh="sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\""
-
 alias cchown="sudo chown -R ucul:ucul"
 alias cde="docker exec -ti"
 alias cgbn="git branch --show-current"
